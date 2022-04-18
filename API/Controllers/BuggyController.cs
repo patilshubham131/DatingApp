3d2f8ce7caf4ab0d96dda19b8aa7e8b53c0d1882
Shubham Patil <patilshubham131@gmail.com>
2022-04-18 21:00:16 +0530

    Added buggy controller to test errors - section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,44 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using API.Entities;
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext context;
+        public BuggyController(DataContext context)
+        {
+            this.context = context;
+        }
+
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret(){
+            return "secret";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(){
+            var thing = context.Users.Find(-1);
+            if(thing == null){
+                return NotFound();
+            }
+        return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError(){
+            var thing = context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest(){
+            return BadRequest("not a good request");
+        }
+    }
+}
